46299cfc316ba1e22360e28f8cb6bb4162b4f5db
Dan Johnson <daniel.johnson@evanta.com>
2014-09-18 07:09:24 -0700

    adds to "other" page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def site_name
     # Change the value below between the quotes.
-    "A Portfolio Site for Dan Johnson"
+    "Development Portfolio | Dan Johnson"
     # "Turtles The Whole Way Down | A Portfolio Site for Dan Johnson"
   end
 
