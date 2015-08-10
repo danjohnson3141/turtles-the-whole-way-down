7cd9267bcd2eda275d3924d64ccc333cff1346a7
Dan Johnson <daniel.johnson@evanta.com>
2015-08-10 14:13:58 -0700

    updates routes, css cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Turtles::Application.routes.draw do
-  get '/about'    => 'high_voltage/pages#show', id: 'about'
-  get '/contact'  => 'high_voltage/pages#show', id: 'contact'
-  get '/privacy'  => 'high_voltage/pages#show', id: 'privacy'
-  get '/terms'    => 'high_voltage/pages#show', id: 'terms'
-  get '/ruby'     => 'high_voltage/pages#show', id: 'ruby'
-  get '/ruby_new' => 'high_voltage/pages#show', id: 'ruby_new'
-  get '/php'      => 'high_voltage/pages#show', id: 'php'
-  get '/resume'   => 'high_voltage/pages#show', id: 'resume'
-  get '/other'    => 'high_voltage/pages#show', id: 'other'
-  get '/blog'    => 'high_voltage/pages#show', id: 'blog'
-  get '/blog/fitness'    => 'high_voltage/pages#show', id: 'blog/fitness'
+  get '/about'          => 'high_voltage/pages#show', id: 'about'
+  get '/contact'        => 'high_voltage/pages#show', id: 'contact'
+  get '/privacy'        => 'high_voltage/pages#show', id: 'privacy'
+  get '/terms'          => 'high_voltage/pages#show', id: 'terms'
+  get '/ruby'           => 'high_voltage/pages#show', id: 'ruby'
+  get '/ruby_new'       => 'high_voltage/pages#show', id: 'ruby_new'
+  get '/php'            => 'high_voltage/pages#show', id: 'php'
+  get '/resume'         => 'high_voltage/pages#show', id: 'resume'
+  get '/other'          => 'high_voltage/pages#show', id: 'other'
+  get '/blog'           => 'high_voltage/pages#show', id: 'blog'
+  get '/blog/fitness'   => 'high_voltage/pages#show', id: 'blog/fitness'
 
   get '/home', to: redirect('/')
 
